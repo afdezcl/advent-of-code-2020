66d79a65399c96a450f2c3b31b371a3a08afa226
afdezcl <adrifdez_@uma.es>
2020-12-06 15:46:32 +0100

    Working on day6

--- src/advent/day6/day6.spec.ts
+++ src/advent/day6/day6.spec.ts
@@ -1,13 +1,18 @@
 import { Day6 } from "./day6";
 
-describe("Test: Day 5", () => {
+describe("Test: Day 6", () => {
 
-    const testDataPath = 'src/advent/day6/data/dataTest.txt';
-    const correctDataPath = 'src/advent/day6/data/data.txt';
+    const testDataPath = 'src/advent/day6/data/dataTest.txt';    
 
     describe("First problem", () => {
         it("Should return the value '11' to data.txt file", () => {
             expect(Day6.problemOne(testDataPath)).toEqual(11);
+        });
+    });
+
+    describe("Second problem", () => {
+        it("Should return the value '6' to data.txt file", () => {
+            expect(Day6.problemTwo(testDataPath)).toEqual(6);
         });
     });
     
--- src/advent/day6/day6.ts
+++ src/advent/day6/day6.ts
@@ -10,16 +10,42 @@ export class Day6 {
     }
 
     static problemOne(dataPath: string): number {
-        let sum = 0;
-        let differentCharacters: string;
+        let questionAnswered = 0;        
         this.settingData((line: string) => {
-            let group = line.replace(/,/g, '');        ;
-            sum += group.replace(/(.)(?=.*\1)/g, "").length;            
+            let group = line.replace(/,/g, '');
+            questionAnswered += group.replace(/(.)(?=.*\1)/g, "").length;            
         }, dataPath);
-        return sum;
+        return questionAnswered;
     }
 
     static problemTwo(dataPath: string) {
-        
+        let group: string[];
+        let questionAnswered = 0;
+        let found = false;        
+        this.settingData((line: string) => {            
+            group = line.split(',');             
+            if(group.length === 1) {
+                //console.log(group[0].length);
+                
+                questionAnswered += group[0].length;
+            } else {                
+                group[0].split('').forEach(letter => {
+                    group.filter(person => person.includes(letter))                    
+                    for(let person = 1; person < group.length; person++) {
+                        //console.log(group[person].includes(letter));
+                        
+                        if(group[person].includes(letter) && !found) {
+                            questionAnswered++;
+                            found = true;
+                        }
+                    }
+                    found = false;
+                });
+                
+            }
+            
+            
+        }, dataPath);
+        return questionAnswered;
     }
 }
